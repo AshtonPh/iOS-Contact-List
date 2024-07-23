27032db628ea663e548d6b199ddf699d3a58d5cd
Ashton Pham <69978620+AshtonPh@users.noreply.github.com>
2024-07-23 15:00:01 +0700

    Basic app creation

---
+++ iOS-Contact-List/iOS Contact list/iOS Contact List/iOS Contact List/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS Contact List
+//
+//  Created by Nam Pham Thanh on 23/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS-Contact-List/iOS Contact list/iOS Contact List/iOS Contact List/iOS_Contact_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Contact_ListApp.swift
+//  iOS Contact List
+//
+//  Created by Nam Pham Thanh on 23/7/24.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Contact_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
